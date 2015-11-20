5f7f18c05527819729e149f60af6e2fd661d3e4d
tsteele1999 <tsteele1999@gmail.com>
2015-11-20 14:04:34 -0600

    League VS Smite
    Basic programming league vs smite.

---
+++ project.php
@@ -0,0 +1,33 @@
+<head>
+  <link rel="stylesheet" type="text/css" href="project_style.css">
+</head>
+
+<h1><center><font color = "#4d4d4d">
+  This is a project created by Bruce and Tyler
+</h1>
+<br>
+
+<div id="textbox">
+  <h1 class="left">
+    Smite
+      </h1>
+      <h1 class="right">
+    League of Legends
+  </h1>
+</div>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<div id=:"checkbox1">
+  <input id="1" type="checkbox" name="1" value="1">
+  <label for="1">Bruce Is A Moose</label>
+</div>
+<br>
+<div id=:"checkbox2">
+  <input id="2" type="checkbox" name="2" value="2">
+  <label for="2"></label>
+</div>
